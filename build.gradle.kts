316d98e6706c4ad47567118d989c77b2f98ae2eb
adnan300d-eng <adnan300d@gmail.com>
2026-05-26 06:49:44 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.1.0"
-    id("org.jetbrains.kotlin.android") version "1.8.10"
+    id("com.android.application") version "8.2.0"
+    id("org.jetbrains.kotlin.android") version "1.9.0"
 }
 
 android {
@@ -20,22 +20,20 @@ android {
     }
     
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
 dependencies {
-    // أساسيات نظام أندرويد وواجهات Compose
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    
-    // إعدادات الواجهة والأيقونات الخاصة بتطبيقك
     implementation("androidx.compose.material3:material3:1.2.0")
     implementation("androidx.compose.material:material-icons-extended:1.6.0")
-    
-    // مكتبة إدارة وقراءة ملفات الهاتف
     implementation("androidx.documentfile:documentfile:1.0.1")
-    
-    // مكتبة الاتصال بالإنترنت والبناء السحابي
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
